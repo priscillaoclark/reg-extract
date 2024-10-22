12ec52e351719af42fae2ae64ae3b5a25501750e
Priscilla Clark <17732979+priscillaoclark@users.noreply.github.com>
2024-10-22 17:06:47 -0400

    occ summaries

--- testing/send_manual_to_ai.py
+++ testing/send_manual_to_ai.py
@@ -5,7 +5,13 @@ import os
 from PyPDF2 import PdfReader
 import pandas as pd
 
-filename = '/Users/bluebird/develop/reg_extract/data/enforcement_actions/documents/fdic/24-0051k.pdf'
+filename = '/Users/bluebird/develop/reg_extract/data/enforcement_actions/documents/occ/eaAA-ENF-2024-79.pdf'   
+
+file = str(filename).split('/')[-1]
+file = file.split('.')[0]
+file = file[2:]
+print(file)
+
 reader = PdfReader(filename)
 count = len(reader.pages)
 
@@ -15,17 +21,17 @@ for i in range(count):
     output.append(page.extract_text())
 
 combined = ','.join(output)
-print(combined)
 ai_summary = get_ai_summaries(combined)
 ai_summary_short, ai_summary_long = ai_summary
 #ai_summary_short = 'test'
 
-file = str(filename).split('/')[-1]
-file = file.split('.')[0]
 data = {'file_name': file, 'summary': ai_summary_short}
 # Convert dictionary to dataframe
 df = pd.DataFrame(data, index=[0])
-print(df.head())
+#print(df.head())
+
+# Save to CSV
+#df.to_csv(f'/Users/bluebird/develop/reg_extract/data/enforcement_actions/occ_{file}.csv', mode='a', header=False, index=False)
 
 from supabase import create_client, Client
 from dotenv import load_dotenv
@@ -42,6 +48,9 @@ try:
 except Exception as e:
     print(f"Error creating Supabase client: {e}")
 
+#url = f'https://occ.gov/static/enforcement-actions/ea{file}.pdf'
+
 # Loop through the dataframe and update the Supabase table
 for index, row in df.iterrows():
-    supabase.table("ea_fdic").update({"ai_summary": row['summary']}).match({"file_name": row['file_name']}).execute()
+    supabase.table("ea_occ").update({"ai_summary": row['summary']}).match({"start_documents": row['file_name']}).execute()
+    #supabase.table("ea_occ").update({"url": url}).match({"start_documents": row['file_name']}).execute()
